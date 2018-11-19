572bbe8c904eba0cc255b28b0e6b6fb752952e59
DOUBLEWIN2 <15029235667@163.com>
2018-11-19 23:32:39 +0800

    三子旗游戏

---
+++ szqgame_9/szqgame_9/game.h
@@ -0,0 +1,11 @@
+#ifndef _GAME_H_
+#define _GAME_H__
+#define ROW 3
+#define COL 3
+void initboard(char board[ROW][COL]);
+void displayboard(char board[ROW][COL]);
+void playermove(char board[ROW][COL]);
+void computermove(char board[ROW][COL]);
+char win(char board[ROW][COL]);
+int full(char board[ROW][COL]);
+#endif
